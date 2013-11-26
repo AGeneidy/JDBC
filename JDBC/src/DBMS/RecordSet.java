d2c2e61c8e7a2f0e29947555a68eeb43ac21eba4
omarbarakat <omarbarakat_92@yahoo.com>
2013-11-26 23:02:17 +0200

    added two constructors to RecordSet, Record: * public RecordSet(RecordSet rs) * public Record(Record record)

--- JDBC/src/DBMS/RecordSet.java
+++ JDBC/src/DBMS/RecordSet.java
@@ -10,6 +10,14 @@ public class RecordSet extends Vector<Record>{		//changed from hashset to vector
 	public RecordSet(String[] attributes) {
 		attributesNames = attributes;
 	}
+	
+	public RecordSet(RecordSet rs) {
+		attributesNames = new String[rs.attributesNames.length];
+		for(int i=0; i<rs.attributesNames.length; i++)
+			this.attributesNames[i] = new String(rs.attributesNames[i]);
+		for(int i=0; i<rs.size(); i++)
+			add(new Record(rs.get(i)));
+	}
 
 	public String[] getAttributesNames() {
 		return attributesNames;
