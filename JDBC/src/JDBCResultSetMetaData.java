9aea687211f9dfdb6a92495440f7e0d964c4aa29
engnaruto <tarekcsed@yahoo.com>
2013-11-28 03:08:26 +0200

    JDBCResultSetMetaData Documented Types Class added

--- JDBC/src/JDBCResultSetMetaData.java
+++ JDBC/src/JDBCResultSetMetaData.java
@@ -3,63 +3,87 @@ import java.sql.SQLException;
 
 public class JDBCResultSetMetaData implements ResultSetMetaData {
 
+	/**
+	 * Returns the number of columns in this ResultSet object.
+	 */
 	@Override
 	public int getColumnCount() throws SQLException {
-
 		return 0;
 	}
 
+	/**
+	 * Gets the designated column's suggested title for use in printouts and
+	 * displays. The suggested title is usually specified by the SQL AS clause.
+	 * If a SQL AS is not specified, the value returned from getColumnLabel will
+	 * be the same as the value returned by the getColumnName method.
+	 */
 	@Override
 	public String getColumnLabel(int column) throws SQLException {
-
 		return null;
 	}
 
+	/**
+	 * Get the designated column's name.
+	 */
 	@Override
 	public String getColumnName(int column) throws SQLException {
-
 		return null;
 	}
 
+	/**
+	 * Retrieves the designated column's SQL type.
+	 */
 	@Override
 	public int getColumnType(int column) throws SQLException {
-
 		return 0;
 	}
 
+	/**
+	 * Gets the designated column's table name.
+	 */
 	@Override
 	public String getTableName(int column) throws SQLException {
-
 		return null;
 	}
 
+	/**
+	 * Indicates whether the designated column is automatically numbered.
+	 */
 	@Override
 	public boolean isAutoIncrement(int column) throws SQLException {
-
 		return false;
 	}
 
+	/**
+	 * Indicates the nullability of values in the designated column.
+	 */
 	@Override
 	public int isNullable(int column) throws SQLException {
-
 		return 0;
 	}
 
+	/**
+	 * Indicates whether the designated column is definitely not writable.
+	 */
 	@Override
 	public boolean isReadOnly(int column) throws SQLException {
-
 		return false;
 	}
 
+	/**
+	 * Indicates whether the designated column can be used in a where clause.
+	 */
 	@Override
 	public boolean isSearchable(int column) throws SQLException {
-
 		return false;
 	}
 
+	/**
+	 * Indicates whether it is possible for a write on the designated column to
+	 * succeed.
+	 */
 	@Override
 	public boolean isWritable(int column) throws SQLException {
-
 		return false;
 	}
 
